8da26bc4ffcf5cef2004f10e413e091405c4d854
michael <michael.john.kilian@gmail.com>
2015-09-12 17:49:15 +0100

    Added templates for all auth functionality. Next step is to switch to HTML5 mode routing and get links put in. Then testing can begin

--- frontend/global_constants/global_constants.js
+++
@@ -1,5 +0,0 @@
-'use_strict';
-
-var app = angular.module('globalConstants', []);
-
-app.constant('REST_BASE_URL','https://dentest.com')
---
+++ server/install_script.sh
@@ -0,0 +1,5 @@
+sudo pip install Django==1.7.8
+sudo pip install django-cors-headers
+sudo pip install djangorestframework
+sudo pip install djoser
+
