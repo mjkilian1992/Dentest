c2a9cef3997220a02cad6cdf90beafbe75320327
michael <michael.john.kilian@gmail.com>
2017-05-28 17:07:29 +0100

    Fixed build errors

--- frontend/partial/home-screen-partial/home-screen-partial.js
+++ frontend/partial/home-screen-partial/home-screen-partial.js
@@ -6,19 +6,19 @@ angular.module('dentest').controller('HomeScreenPartialCtrl',function($scope){
     self.slides = [];
 
   self.slides.push({
-    image: 'static/students_at_computer.jpg',
+    image: 'img/students_at_computer.jpg',
     title: 'Over 6000 questions.',
     text: 'Dentest contains a bank of over 6000 questions covering a wide range of Dentistry related topics.',
   });
 
   self.slides.push({
-    image: 'static/dentists.jpg',
+    image: 'img/dentists.jpg',
     title: 'Validated by experts.',
     text: 'All of our questions are validated by professional dentists and dentistry teachers to ensure accuracy and relevance.',
   });
 
   self.slides.push({
-    image: 'static/students.jpg',
+    image: 'img/students.jpg',
     title: 'Study on the move.',
     text: 'Dentest is perfect for revising and learning while on the move or when you only have a short break.'
   });
