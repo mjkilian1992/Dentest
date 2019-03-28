5e6baad2b827f7598f3abb8dda4559b2285cf261
Michael Kilian <michael.john.kilian@gmail.com>
2019-03-28 13:59:42 +0000

    Update settings_prod.py

--- server/dentest/settings_prod.py
+++ server/dentest/settings_prod.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# Replace with fresh key before using
 SECRET_KEY = 'q&(ymk$u0)=g9e3wukv6@qkly$&c-$=07ds#&jnt=0wicm@o^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
